f9a75e3652e9c68a648d9d01006315e5dc824fed
ngabo-dev <j.niyongabo@alustudent.com>
2024-05-18 03:15:51 -0700

    done

--- minimum_operations/0-minoperations.py
+++ minimum_operations/0-minoperations.py
@@ -2,20 +2,18 @@
 """ Minimum Operations
     """
 
-
 def minOperations(n: int) -> int:
     """ Minimum Operations needed to get n H characters """
-    next = 'H'
-    body = 'H'
-    op = 0
-    while (len(body) < n):
-        if n % len(body) == 0:
-            op += 2
-            next = body
-            body += body
-        else:
-            op += 1
-            body += next
-    if len(body) != n:
+    if n <= 1:
         return 0
-    return op
+    
+    operations = 0
+    factor = 2
+    
+    while n > 1:
+        while n % factor == 0:
+            operations += factor
+            n //= factor
+        factor += 1
+    
+    return operations
