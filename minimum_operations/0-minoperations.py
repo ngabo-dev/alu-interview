003f603369784a77785fe79666854221cd90c641
ngabo-dev <j.niyongabo@alustudent.com>
2024-05-18 03:19:55 -0700

    done

--- minimum_operations/0-minoperations.py
+++ minimum_operations/0-minoperations.py
@@ -2,18 +2,19 @@
 """ Minimum Operations
     """
 
+
 def minOperations(n: int) -> int:
     """ Minimum Operations needed to get n H characters """
     if n <= 1:
         return 0
-    
+
     operations = 0
     factor = 2
-    
+
     while n > 1:
         while n % factor == 0:
             operations += factor
             n //= factor
         factor += 1
-    
+
     return operations
