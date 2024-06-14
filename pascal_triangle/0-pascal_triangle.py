a7eefea0738f039cb38ee278f90d8d9fc940c8b4
ngabo-dev <j.niyongabo@alustudent.com>
2024-06-14 09:41:06 -0700

    pascal

--- pascal_triangle/0-pascal_triangle.py
+++ pascal_triangle/0-pascal_triangle.py
@@ -6,7 +6,7 @@
 
 def pascal_triangle(n):
   """
-  This function generates a list of lists representing Pascal's Triangle up to level n.
+  Generates a list of lists representing Pascal's Triangle up to level n.
 
   Args:
       n: An integer representing the desired level of Pascal's Triangle.
@@ -15,20 +15,17 @@ def pascal_triangle(n):
       A list of lists containing the elements of Pascal's Triangle for level n.
       An empty list if n is less than or equal to 0.
   """
-
   if n <= 0:
     return []
 
-  triangle = [[1]]  # Initialize the triangle with the first row (always [1])
+  triangle = [[1]]
 
-  # Iterate for each row from level 2 to n
   for i in range(1, n):
-    previous_row = triangle[i-1]  # Access the previous row
+    previous_row = triangle[i-1]
     current_row = []
-    # Build the current row based on the previous row
     for j in range(i + 1):
       if j == 0 or j == i:
-        current_row.append(1)  # First and last elements are always 1
+        current_row.append(1)
       else:
         element = previous_row[j-1] + previous_row[j]
         current_row.append(element)
